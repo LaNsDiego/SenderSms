26551e51b42cec97f9417368038be411d6dd39ed
Diego Layme <vandiego_pandax@hotmail.com>
2021-01-06 23:53:01 -0500

    detail producto developing

---
+++ app/src/main/java/com/example/sendersms/views/EntradaActivity.java
@@ -0,0 +1,16 @@
+package com.example.sendersms.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sendersms.R;
+
+public class EntradaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada);
+    }
+}
